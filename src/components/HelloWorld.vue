45ee9f8e73e3b2d05ff4dabf0fa15a1fa9214381
David Long <davdaarn@users.noreply.github.com>
2020-12-24 10:36:58 -0700

    basic navigation working for windows

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <v-btn outlined v-on:click="getPath">Test</v-btn>
-    <v-btn outlined v-on:click="back">Back</v-btn>
+    <v-btn outlined v-on:click="directoryUp">Back</v-btn>
+    <v-btn outlined v-on:click="logDir">Directory</v-btn>
     <div>
       <span>Directory: </span>
       <span>{{ homedir }}</span>
@@ -18,7 +19,11 @@
       top
       color="deep-purple accent-4"
     ></v-progress-linear>
-    <div v-for="(d, i) in directories" :key="i" v-on:click="navigate(d)">
+    <div
+      v-for="(d, i) in directoryItems"
+      :key="i"
+      v-on:click="d.type !== 'file' ? directoryDown(d) : null"
+    >
       <v-icon>{{ d.icon }}</v-icon>
       {{ d.path }}
     </div>
@@ -38,12 +43,15 @@ export default {
   data() {
     return {
       homedir: '',
-      directories: [],
+      directoryItems: [],
       isLoading: false,
-      currentDir: '/'
+      currentDir: []
     };
   },
   methods: {
+    logDir() {
+      console.log(this.currentDir);
+    },
     async getPath() {
       this.homedir = os.homedir();
 
@@ -75,54 +83,137 @@ export default {
       // const drives = await drivelist.list();
       // console.log(drives);
     },
-    async navigate(dir) {
-      console.log(dir);
-      // this.currentDir = dir;
-      // this.directories = [];
-      // let paths = fs.readdirSync(dir);
-      // paths.forEach(p => {
-      //   try {
-      //     p = path.join(dir, p);
-      //     const stat = fs.statSync(p);
-      //     this.directories.push(p);
-      //   } catch (error) {
-      //     console.log(error);
-      //   }
-      // });
+    // async navigate(dir, back = false) {
+    //   // console.log(dir);
+    //   // console.log(this.currentDir);
+
+    //   if (!back && dir !== '' && dir) {
+    //     this.currentDir += `${dir.path}\\`;
+    //   } else {
+    //     this.currentDir = dir;
+    //   }
+    //   console.log(this.currentDir);
+    //   this.directoryItems = [];
+    //   let paths = fs.readdirSync(this.currentDir, { withFileTypes: true }); // returns array of dirnet
+    //   // console.log(paths);
+    //   paths.forEach(p => {
+    //     // console.log(p.isDirectory());
+    //     try {
+    //       if (p.isDirectory()) {
+    //         this.directoryItems.push(
+    //           new DirectoryItem({
+    //             path: p.name,
+    //             // type: getType(r),
+    //             type: 'system',
+    //             icon: 'mdi-folder-text-outline'
+    //           })
+    //         );
+    //       } else if (p.isFile()) {
+    //         this.directoryItems.push(
+    //           new DirectoryItem({
+    //             path: p.name,
+    //             // type: getType(r),
+    //             type: 'file',
+    //             icon: 'mdi-file-music-outline'
+    //           })
+    //         );
+    //       }
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   });
+    // },
+
+    async updateDirectoryItems(fullPath) {
+      this.directoryItems = [];
+      let paths = fs.readdirSync(fullPath, { withFileTypes: true }); // returns array of dirnet
+      // console.log(paths);
+      paths.forEach(p => {
+        // console.log(p.isDirectory());
+        try {
+          console.log(p);
+          if (p.isDirectory()) {
+            this.directoryItems.push(
+              new DirectoryItem({
+                path: p.name,
+                // type: getType(r),
+                type: 'system',
+                icon: 'mdi-folder-text-outline'
+              })
+            );
+          } else if (p.isFile()) {
+            this.directoryItems.push(
+              new DirectoryItem({
+                path: p.name,
+                // type: getType(r),
+                type: 'file',
+                icon: 'mdi-file-music-outline'
+              })
+            );
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      });
     },
-    async back() {
-      console.log(this.currentDir.mountpoint);
-      let temp = this.currentDir.mountpoint.split('\\');
-      console.log(temp);
-      if (temp.length > 1) {
-        temp.splice(-1, 1);
-        this.navigate(temp.join('\\'));
+    async directoryDown(nextDir) {
+      // push nextDir to currentDir array
+      this.currentDir.push(nextDir.path);
+      // create full path from currentDir
+      const fullPath = this.currentDir.join('\\').replace('\\\\', '\\');
+      // readdirSync using full path
+      // update current list of directoryItems
+      this.updateDirectoryItems(fullPath);
+    },
+    async directoryUp() {
+      // pop last element in currentDir
+      if (this.currentDir.length > 1) {
+        this.currentDir.pop(-1, 1);
+        // create full path from currentDir
+        const fullPath = this.currentDir.join('\\').replace('\\\\', '\\');
+        // readdirSync using full path
+        // update current list of directoryItems
+        this.updateDirectoryItems(fullPath);
       } else {
-        this.navigate(temp[0] + '\\');
+        this.toRoot();
       }
-    }
-  },
-  created() {
-    this.$nextTick(() => {
+    },
+    // navigateTo(fullPath)
+    async toRoot() {
       // ipc.on('drives', (e, m) => {
       //   console.log(e, m);
-      //   this.directories = m;
+      //   this.directoryItems = m;
       // });
       this.isLoading = true;
       ipc
         .invoke('findAllMountedDrives')
         .then(res => {
           console.log(res);
-          let temp = res.map(r => {
-            return new DirectoryItem({
-              path: r.mountpoints[0].path,
-              // type: getType(r),
-              type: 'system',
-              icon: 'mdi-folder-text-outline'
+          let temp = res
+            .filter(x => !x.error)
+            .map(r => {
+              console.log(!r.error);
+              switch (r.busType) {
+                case 'USB':
+                  return new DirectoryItem({
+                    path: r.mountpoints[0].path,
+                    // type: getType(r),
+                    type: r.busType,
+                    icon: 'mdi-usb-port'
+                  });
+                case 'SATA':
+                  return new DirectoryItem({
+                    path: r.mountpoints[0].path,
+                    // type: getType(r),
+                    type: r.busType,
+                    icon: 'mdi-harddisk'
+                  });
+                default:
+                  return;
+              }
             });
-          });
-
-          this.directories = temp.sort((a, b) => {
+          console.log(temp);
+          this.directoryItems = temp.sort((a, b) => {
             if (a.path < b.path) {
               return -1;
             }
@@ -137,8 +228,14 @@ export default {
           // setTimeout(() => {
           //   this.isLoading = false;
           // }, 5000);
+          this.currentDir = [];
           this.isLoading = false;
         });
+    }
+  },
+  created() {
+    this.$nextTick(() => {
+      this.toRoot();
     });
   }
 };
